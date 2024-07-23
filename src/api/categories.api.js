b2beffe04393e8bd100a8f017bdef75ff4773dd5
brandonlee2110 <baodgph30207@fpt.edu.vn>
2024-07-23 12:41:44 +0700

    feat: thêm category api

--- src/api/categories.api.js
+++ src/api/categories.api.js
@@ -11,3 +11,48 @@ export const getCategories = async (pagination) => {
     console.log(error);
   }
 };
+
+export const addNewCategory = async (data) => {
+  try {
+    const response = await axios.post('http://localhost:8000/api/v1/categories', data);
+
+    console.log('response', response);
+
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+  return null;
+
+  // You may want to handle the error differently depending on your application's requirements
+};
+
+export const deleteCategory = async (id) => {
+  try {
+    const response = await axios.delete(`http://localhost:8000/api/v1/categories/${id}`);
+
+    console.log('response', response);
+
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+  return null;
+  // You may want to handle the error differently depending on your application's requirements
+  // For example, you might want to display an error message to the user
+};
+
+export const update = async (id, data) => {
+  try {
+    const response = await axios.put(`http://localhost:8000/api/v1/categories/${id}`, data);
+
+    console.log('response', response);
+
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+  return null;
+  // You may want to handle the error differently depending on your application's requirements
+  // For example, you might want to display an error message to the user
+};
